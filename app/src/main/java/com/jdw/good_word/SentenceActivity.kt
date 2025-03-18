d0cca62b55dd442f1b94ccd6e7b04b8ca78cb061
Dalso13 <flskwhdhk@gmail.com>
2025-03-18 13:25:36 +0900

    feat: button 클릭으로 페이지 이동

---
+++ app/src/main/java/com/jdw/good_word/SentenceActivity.kt
@@ -0,0 +1,14 @@
+package com.jdw.good_word
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SentenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sentence)
+    }
+}
